da8c866fd88946b4b044c4447a57ab7fecff73ab
Shakir <48360417+i360ss@users.noreply.github.com>
2022-08-06 00:07:33 +0530

    Session and Error fixes

--- public_html/index.php
+++ public_html/index.php
@@ -1,7 +1,6 @@
 <?php
 require __DIR__.'/../vendor/autoload.php';
 
-use Ozz\Core\Errors;
 use Ozz\Core\Appinit;
 use Ozz\Core\Middleware;
 
@@ -10,9 +9,6 @@ $APP = new Appinit();
 require __DIR__.'/../app/Error.php';
 require __DIR__.'/../app/Roles.php';
 require __DIR__.'/../app/Route.php';
-
-$errors = new Errors();
-$GLOBAL['errors'] = $errors;
 
 Middleware::execute();
 $APP->run();
