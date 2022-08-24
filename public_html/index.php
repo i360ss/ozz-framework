f04cea05235b84d6ef59eb0307d270d6400c48a3
i360ss <shakeerwahid76@gmail.com>
2022-08-24 13:23:21 +0530

    Debug bar initial

--- public_html/index.php
+++ public_html/index.php
@@ -3,14 +3,16 @@ require __DIR__.'/../vendor/autoload.php';
 
 use Ozz\Core\Appinit;
 use Ozz\Core\Middleware;
+use Ozz\Core\DebugBar;
 
+$DEBUG_BAR = new DebugBar();
 $APP = new Appinit();
 
-require __DIR__.'/../app/Error.php';
 require __DIR__.'/../app/Roles.php';
 require __DIR__.'/../app/Route.php';
+require __DIR__.'/../app/Error.php';
 
 Middleware::execute();
 $APP->run();
-
+DEBUG ? $DEBUG_BAR->show() : false;
 ?>
