37d8a3f0f3a35fe0bcafe451eb5a85eb1037d4d8
i360ss <shakeerwahid76@gmail.com>
2022-08-25 01:21:59 +0530

    Register middleware file cleanup

--- app/RegisterMiddleware.php
+++ app/RegisterMiddleware.php
@@ -1,24 +1,27 @@
 <?php
 /**
  * REGISTER MIDDLEWARES
- * 
  * You should register all your middlewares here to load
- * $route_middlewares --> This should be called from route to excecute
- * $auto_middlewares  --> This will excecute automatically on all requests
- * 
  */
 
 
 use App\middleware\AuthMiddleware;
+use App\middleware\Test;
 
 
-$route_middlewares = [
-  'auth' => AuthMiddleware::class,
+/**
+ * This will execute automatically on all requests
+ */
+$auto_middlewares = [
+  'Test' => Test::class,
 
 ];
 
 
-$auto_middlewares = [
-  'test'
-
+/**
+ * This should be called from route to execute
+ */
+$route_middlewares = [
+  'auth' => AuthMiddleware::class,
+  
 ];
---
+++ app/middleware/Test.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\middleware;
+
+use Ozz\Core\Request;
+
+class Test {
+
+  public function handle(){
+    echo 'Test Middleware loaded <br>';
+    
+  }
+
+}
