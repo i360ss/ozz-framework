adbaeb53aa694113963a4e61e41f1b598e9d06dd
Shakir <shakeerwahid76@gmail.com>
2023-03-08 19:03:50 +0530

    Exception and app/codegen file created

---
+++ app/codegen.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Ozz file generating commands will use this functions to get contents at first
+ * It will use the default fallback contents if any of below functions are not defined or commented.
+ * You can define the code for the following files: (view, base-layout, controller, model, component, middleware, email-template)
+ */
+
+
+/**
+ * View code (app/view)
+ * @param string $data (name, path)
+ */
+// function ozz_code_gen_view($data){
+//   $data['name'];
+//   $data['path'];
+
+//   return '';
+// }
+
+
+
+/**
+ * Base layout code (app/view/base)
+ * @param string $data (name, path)
+ */
+// function ozz_code_gen_layout($data){
+//   $data['name'];
+//   $data['path'];
+
+//   return '';
+// }
+
+
+
+/**
+ * Component (app/view/components)
+ * @param string $data (name, path)
+ */
+// function ozz_code_gen_component($data){
+//   $data['name'];
+//   $data['path'];
+
+//   return '';
+// }
+
+
+
+/**
+ * Controller code (app/controller)
+ * @param string $data (namespace, class)
+ */
+// function ozz_code_gen_controller($data){
+//   $data['namespace'];
+//   $data['class'];
+
+//   return '';
+// }
+
+
+
+/**
+ * Model code (app/model)
+ * @param string $data (namespace, class)
+ */
+// function ozz_code_gen_model($data){
+//   $data['namespace'];
+//   $data['class'];
+
+//   return '';
+// }
+
+
+
+/**
+ * Middleware code (app/model)
+ * @param string $data (namespace, class)
+ */
+// function ozz_code_gen_middleware($data){
+  // $data['namespace'];
+  // $data['class'];
+
+//   return '';
+// }
+
+
+
+/**
+ * Email template code (app/email_template)
+ * @param string $data (name, path)
+ */
+// function ozz_code_gen_email_template($data){
+//   $data['name'];
+//   $data['path'];
+
+//   return 'Path: '.$data['path'].' Name: '.$data['name'];
+// }
+
+
+?>
--- public_html/index.php
+++ public_html/index.php
@@ -6,7 +6,9 @@ use Ozz\Core\AppInit;
 use Ozz\Core\Middleware;
 use Ozz\Core\DebugBar;
 use Ozz\Core\Request;
+use Ozz\Core\OzzExceptions;
 
+$exception = new OzzExceptions();
 $DEBUG_BAR = new DebugBar();
 $APP = new AppInit();
 
