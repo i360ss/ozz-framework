2616369322f7829c14f13b5557b0b2db78ab3b1d
i360ss <shakeerwahid76@gmail.com>
2025-01-04 13:27:34 +0530

    CMS default route added + env file update

--- app/Route.php
+++ app/Route.php
@@ -4,6 +4,17 @@ use Ozz\Core\Request;
 use Ozz\Core\Response;
 use Ozz\Core\Auth;
 
+/**
+ * Uncomment below route if you using Ozz CMS
+ */
+// Router::get('/{slug}', function(Request $request) {
+//   if (get_post($request->urlParam('slug'))) {
+//     get_cms_page($request->urlParam('slug'));
+//   } else {
+//     return render_error_page();
+//   }
+// });
+
 Router::get('/', function(){
   $data = [
     'greeting' => 'Welcome to <span>ozz</span>',
@@ -18,10 +29,5 @@ Router::get('/', function(){
 });
 
 Router::get('/api', function(){
-  $data = [
-    'name' => 'John Doe',
-    'age' => 26
-  ];
-
-  return json($data);
+  return json( [ 'name' => 'John Doe', 'age' => 26 ] );
 });
