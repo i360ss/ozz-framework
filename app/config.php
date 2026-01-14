5f24528cc26e895ce05c7f3a9766e1379dc62b27
i360ss <shakeerwahid76@gmail.com>
2026-01-14 14:09:57 +0530

    Config value (session_timeout) removed

--- app/config.php
+++ app/config.php
@@ -3,7 +3,6 @@ return [
   // General Config
   'CHARSET' => 'utf-8',
   'SESSION_DRIVER' => 'memory',
-  'SESSION_LIFETIME' => 20000,
   'AUTH_USER_ROLES' => [
     'admin' => [
       'landing_page' => '/admin'
