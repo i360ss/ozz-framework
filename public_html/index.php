137025fa4876d39bb2db1066e58599f062c435cf
i360ss <shakeerwahid76@gmail.com>
2026-06-10 00:37:01 +0530

    Undo route pointing on previous release - Core update

--- public_html/index.php
+++ public_html/index.php
@@ -1,11 +1,4 @@
 <?php
-if (php_sapi_name() === 'cli-server') {
-    $file = __DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    if (is_file($file)) {
-        return false;
-    }
-}
-
 require __DIR__.'/../vendor/autoload.php';
 require __DIR__.'/../app/bootstrap.php';
 ?>
