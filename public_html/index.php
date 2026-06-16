38b3f0b79f7e73c6862fa77d109b0104f719bfde
i360ss <shakeerwahid76@gmail.com>
2026-06-16 09:27:58 +0530

    Update dependencies and refactor file path definitions

--- public_html/index.php
+++ public_html/index.php
@@ -1,4 +1,9 @@
 <?php
-require __DIR__.'/../vendor/autoload.php';
-require __DIR__.'/../app/bootstrap.php';
+defined('BASE_DIR') || define('BASE_DIR', __DIR__.'/../');
+defined('ENV_FILE') || define('ENV_FILE', __DIR__.'/../env.ini');
+defined('CSP_FILE') || define('CSP_FILE', __DIR__.'/../csp.ini');
+defined('CONFIG_FILE') || define('CONFIG_FILE', __DIR__.'/../app/config.php');
+
+require BASE_DIR.'/vendor/autoload.php';
+require BASE_DIR.'/app/bootstrap.php';
 ?>
