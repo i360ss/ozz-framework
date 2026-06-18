e7a905195a5fbc14a22574a27072d65873bba203
i360ss <shakeerwahid76@gmail.com>
2026-06-18 11:57:44 +0530

    App config moved to index.php + page caching optimized

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,6 +4,12 @@ use Ozz\Core\Middleware;
 use Ozz\Core\DebugBar;
 use Ozz\Core\Request;
 use Ozz\Core\ExceptionHandler;
+use Ozz\Core\Cache;
+
+// Return page cache if available
+if(CONFIG['PAGE_CACHE_LIFETIME'] && $page_cache = (new Cache)->get()){
+  exit($page_cache);
+}
 
 $APP = new AppInit();
 $EXCEPTION = new ExceptionHandler();
--- public_html/index.php
+++ public_html/index.php
@@ -2,7 +2,11 @@
 defined('BASE_DIR') || define('BASE_DIR', __DIR__.'/../');
 defined('ENV_FILE') || define('ENV_FILE', __DIR__.'/../env.ini');
 defined('CSP_FILE') || define('CSP_FILE', __DIR__.'/../csp.ini');
-defined('CONFIG_FILE') || define('CONFIG_FILE', __DIR__.'/../app/config.php');
+
+// App config
+$devConfig = include BASE_DIR.'/app/config.php';
+$defConfig = include BASE_DIR.'/vendor/ozz/core/src/system/default-config.php';
+define('CONFIG', array_replace_recursive($defConfig, $devConfig));
 
 require BASE_DIR.'/vendor/autoload.php';
 require BASE_DIR.'/app/bootstrap.php';
