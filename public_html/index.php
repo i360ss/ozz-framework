ca76eb03506aeca9bf11054314ead58ea7bf5c30
i360ss <shakeerwahid76@gmail.com>
2026-06-20 23:45:24 +0530

    Core version update and minor changes

--- public_html/index.php
+++ public_html/index.php
@@ -2,7 +2,6 @@
 defined('ROOT') || define('ROOT', __DIR__);
 defined('BASE_DIR') || define('BASE_DIR', __DIR__.'/../');
 defined('ENV_FILE') || define('ENV_FILE', __DIR__.'/../env.ini');
-defined('CSP_FILE') || define('CSP_FILE', __DIR__.'/../csp.ini');
 
 $devConfig = include BASE_DIR.'/app/config.php';
 $defConfig = include BASE_DIR.'/vendor/ozz/core/src/system/default-config.php';
